ff08dc304c74af792f78d2ba7c1f204b19e350e5
BIThooven <yassine.parkourist@gmail.com>
2023-10-02 18:07:26 +0100

    check for cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - a function to detect a loop in a linked list
+ * @list: a typedef struct (the linked list)
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *prev, *forward;
+
+	prev  = list;
+	forward = list;
+	for (; prev != NULL && forward != NULL && prev->next != NULL; )
+	{
+		prev = prev->next;
+		forward = forward->next->next;
+
+		if (prev == forward)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
