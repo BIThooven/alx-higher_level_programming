ebd418cb7ff48b62b8536e59a94b478184904ffb
BIThooven <yassine.parkourist@gmail.com>
2023-10-24 15:29:15 +0100

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''A Square Class'''
+
+
+class Square:
+    '''A Class to define Square'''
+    def __init__(self, size):
+        '''Instantiation with size
+        Args:
+            size (int): The size of the square
+        '''
+        self.__size = size
