824f82c141ae42623fc90cb9c4233d067070fb3d
BIThooven <yassine.parkourist@gmail.com>
2023-10-24 16:23:11 +0100

    square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Create a Class for Square size'''
+
+
+class Square:
+    '''Class of a Square'''
+
+    def __init__(self, size=0):
+        '''Instantiaion of a square with size
+        Args:
+            size (int): The size of the square
+        '''
+        if (type(size) is not int):
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
