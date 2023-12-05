eb8137dc047d585d240d1c9eb1cfed74a18a8a77
BIThooven <yassine.parkourist@gmail.com>
2023-12-05 23:49:51 +0100

    stduents class to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Students class"""
+
+
+class Student:
+    """Represents a student."""
+    def __init__(self, first_name, last_name, age):
+        """initializing a student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of the Student instance."""
+        return self.__dict__
