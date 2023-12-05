c2b48b8a3d7110fed061d5156c9fef59b71cafa7
BIThooven <yassine.parkourist@gmail.com>
2023-12-05 16:46:14 +0100

    get data from json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""python data structure returned from a json string"""
+import json
+
+
+def from_json_string(my_str):
+    """a function to retrieve data from a json file
+    
+    Args:
+        my_str: a json string
+    Return:
+        returns python data from a json string
+    """
+    return json.loads(my_str)
