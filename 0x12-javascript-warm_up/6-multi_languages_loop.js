14282646fe8e98c48b2b5bc306f929c282efa9f9
BIThooven <yassine.parkourist@gmail.com>
2024-02-12 22:27:46 +0100

    loop in array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let count = 0;
 for (count in array) {
   console.log(array[count++]);
