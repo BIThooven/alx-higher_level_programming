4fea56a5decf87abb1fd7238dedf123cb2a79fd6
BIThooven <yassine.parkourist@gmail.com>
2024-02-12 15:19:48 +0100

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const keyarg = process.argv.length - 2;
+
+if (keyarg === 0) {
+  console.log('No argument');
+} else if (keyarg === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
