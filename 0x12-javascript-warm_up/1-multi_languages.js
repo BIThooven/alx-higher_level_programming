cf62795a582cf2766970b75b2d127c32a8044c53
BIThooven <yassine.parkourist@gmail.com>
2024-02-12 14:48:17 +0100

    3-languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const C = 'C is fun';
+const python = 'Python is cool';
+const Javascript = 'JavaScript is amazing';
+
+console.log(C);
+console.log(python);
+console.log(Javascript);
