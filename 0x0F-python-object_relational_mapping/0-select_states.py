839b9876b18df4208e31bee403faeb7457577758
BIThooven <yassine.parkourist@gmail.com>
2024-02-16 15:11:43 +0100

    get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,18 +2,21 @@
 import MySQLdb
 import sys
 """listing states from the database hbtn_0e_0_usa"""
+
+
 def main():
     """
         Function containing code to select all the states
         from the database.
     """
 
+    # Create a database connection
     conn = MySQLdb.connect(
                 host="localhost", port=3306, user=sys.argv[1],
                 passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
             )
     cur = conn.cursor()
-
+    # Select states
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
