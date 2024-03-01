36f91297d77a0eb6546da8def68b169f15f0669a
BIThooven <yassine.parkourist@gmail.com>
2024-03-01 17:55:55 +0100

    POST an emai

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" POST an email"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    response = requests.post(url, data={'Your email is': email})
+    print(response.text)
