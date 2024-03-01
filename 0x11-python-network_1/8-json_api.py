841b1f7f9c70468d0ded5d0a876b209822e85457
BIThooven <yassine.parkourist@gmail.com>
2024-03-01 18:18:12 +0100

    search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,16 +5,19 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "http:////0.0.0.0:5000/search_user"
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
-    response = requests.post(url, data={'q': q})
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+
+    response = requests.post(url, data=data)
     try:
-        response = response.json()
-        if response:
-            print("[{}] {}".format(response.get('id'), response.get('name')))
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
         else:
             print("No result")
     except ValueError:
