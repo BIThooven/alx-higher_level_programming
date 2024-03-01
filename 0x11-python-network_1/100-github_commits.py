4d926e43c15d763239c2c2147042ba8bb77a671e
BIThooven <yassine.parkourist@gmail.com>
2024-03-01 23:15:26 +0100

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""10 github commits"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
