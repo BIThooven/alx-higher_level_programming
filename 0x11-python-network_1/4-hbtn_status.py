aae5e0e6c728ba222751ae7f8ad32d957d983679
BIThooven <yassine.parkourist@gmail.com>
2024-03-01 17:44:58 +0100

    fetching with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""a Python script that fetches https://intranet.hbtn.io/status"""
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
