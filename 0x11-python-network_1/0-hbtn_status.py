3961a8eea0d064bec537f7ebf6bd9e8f4c7a096d
BIThooven <yassine.parkourist@gmail.com>
2024-03-01 14:53:01 +0100

    fetching

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """Fetches https://intranet.hbtn.io/status"""
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
